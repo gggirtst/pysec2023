4ed9d4b4b12cba9a025526c1594f093b52e22ce3
gggirtst <144527386+gggirtst@users.noreply.github.com>
2023-10-08 23:08:57 +0300

    Update ex6_main.py

--- exercises/ex6_main.py
+++ exercises/ex6_main.py
@@ -1,11 +1,44 @@
 import ex6_configuration
 
 user_hostname = input("Enter hostname: ")
-user_gateway = input("Set default gateway for the device: ")
 user_vlan_id = input("Set vlan (for example 'Vlan20'): ")
-user_ip = input("Set IP address for Vlan interface: ")
 user_interface = input("Set interface for Vlan trunking: ")
 
+def is_valid_ip(ip_str):
+    # Split the IP address into octets
+    octets = ip_str.split('.')
+    
+    # Check if there are exactly 4 octets
+    if len(octets) != 4:
+        return False
+    
+    try:
+        # Check if each octet is an integer in the range [0, 255]
+        for octet in octets:
+            octet_int = int(octet)
+            if octet_int < 0 or octet_int > 255:
+                return False
+    except ValueError:
+        # If any octet is not an integer, it's not a valid IP address
+        return False
+    
+    return True
+
+while True:
+    user_ip = input("Set IP address for Vlan interface: ")
+    user_gateway = input("Set default gateway for the device: ")
+
+    if is_valid_ip(user_ip):
+        print("Valid IP address:", user_ip)
+        break
+    else:
+        print("Invalid IP address. Please enter a valid IP address.")
+    
+    if is_valid_ip(user_gateway):
+        print("Valid IP address:", user_gateway)
+        break
+    else:
+        print("Invalid IP address. Please enter a valid IP address.")
 
 device = ex6_configuration.create_config(user_hostname)
 ex6_configuration.set_default_route(device, user_gateway)
